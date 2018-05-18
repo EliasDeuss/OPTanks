a160284b94a8702a618d26c89210f1526d5f17ff
Elias <eliasbdeuss@gmail.com>
2018-05-18 11:19:09 -0700

    ReSkined buttons

--- src/Game.java
+++ src/Game.java
@@ -43,6 +43,11 @@ public class Game implements ActionListener, KeyListener {
 	private ImageIcon imgStartMain = new ImageIcon(getClass().getResource("startMain.png"));
 	private ImageIcon imgLeadMain = new ImageIcon(getClass().getResource("leadMain.png"));
 	private ImageIcon imgSettMain = new ImageIcon(getClass().getResource("settMain.png"));
+	private ImageIcon imgSGMain = new ImageIcon(getClass().getResource("sgMain.png"));
+	private ImageIcon imgNormalMain = new ImageIcon(getClass().getResource("normalMain.png"));
+	private ImageIcon imgPlMain = new ImageIcon(getClass().getResource("plMain.png"));
+	private ImageIcon imgOnePlMain = new ImageIcon(getClass().getResource("OnePlMain.png"));
+	private ImageIcon imgTwoPlMain = new ImageIcon(getClass().getResource("TwoPlMain.png"));
 	
 	//Players
 	private ImageIcon imgTank1 = new ImageIcon(getClass().getResource("tank1.png"));
@@ -91,6 +96,222 @@ public class Game implements ActionListener, KeyListener {
 		gameFrame.getContentPane().setBackground(Color.WHITE);
 		
 		gameFrame.setVisible(true);
+	}
+	
+	public void StartScreen()
+	{
+		lblGameTitle = new JLabel(imgLogoMain);
+		lblGameTitle.setSize(310, 70);
+		lblGameTitle.setFont(new Font("Serif", Font.PLAIN, 39));
+		lblGameTitle.setLocation(280, 100);
+		lblGameTitle.setOpaque(false);
+		lblGameTitle.setVisible(true);
+		
+		startGame = new JButton(imgStartMain);
+		startGame.setSize(181, 46);
+		startGame.setFont(new Font("Serif", Font.PLAIN, 25));
+		startGame.setLocation(345, 170);
+		startGame.setFocusable(false);
+		startGame.setActionCommand("startGame");
+		startGame.addActionListener(
+				  new ActionListener() 
+						{
+					    public void actionPerformed(ActionEvent e) 
+					    {
+					    	if (e.getActionCommand().equals("startGame"))
+							{
+					    		hideStartScreen();
+								gamemodeSelector();
+							}
+						}
+				  }
+				);
+		startGame.setVisible(true);
+		
+		leaderboardMainBTN = new JButton(imgLeadMain);
+		leaderboardMainBTN.setFont(new Font("Serif", Font.PLAIN, 25));
+		leaderboardMainBTN.setSize(181, 46);
+		leaderboardMainBTN.setLocation(345, 219);
+		leaderboardMainBTN.setFocusable(false);
+		leaderboardMainBTN.setActionCommand("leaderBoard");
+		//startGame.addActionListener(this);
+		leaderboardMainBTN.setVisible(true);
+		
+		settingsMainBTN = new JButton(imgSettMain);
+		settingsMainBTN.setFont(new Font("Serif", Font.PLAIN, 25));
+		settingsMainBTN.setSize(179, 40);
+		settingsMainBTN.setLocation(345, 269);
+		settingsMainBTN.setFocusable(false);
+		settingsMainBTN.setActionCommand("settings");
+		//startGame.addActionListener(this);
+		settingsMainBTN.setVisible(true);
+		
+		//Add Items to Frame
+		gameFrame.add(lblGameTitle);
+		gameFrame.add(startGame);
+		gameFrame.add(leaderboardMainBTN);
+		gameFrame.add(settingsMainBTN);
+	}
+	
+	public void hideStartScreen()
+	{
+		lblGameTitle.setVisible(false);
+		startGame.setVisible(false);
+		leaderboardMainBTN.setVisible(false);
+		settingsMainBTN.setVisible(false);
+	}
+	
+	public void gamemodeSelector()
+	{
+		lblGameSelectorTitle = new JLabel(imgSGMain);
+		lblGameSelectorTitle.setSize(300, 45);
+		lblGameSelectorTitle.setFont(new Font("Serif", Font.PLAIN, 35));
+		lblGameSelectorTitle.setLocation(290, 100);
+		lblGameSelectorTitle.setOpaque(false);
+		lblGameSelectorTitle.setVisible(true);
+		
+		normalGamemodeBTN = new JButton(imgNormalMain);
+		normalGamemodeBTN.setSize(180, 40);
+		normalGamemodeBTN.setFont(new Font("Serif", Font.PLAIN, 25));
+		normalGamemodeBTN.setLocation(345, 170);
+		normalGamemodeBTN.setFocusable(false);
+		normalGamemodeBTN.setActionCommand("normalGame");
+		normalGamemodeBTN.addActionListener(
+				  new ActionListener() 
+						{
+					    public void actionPerformed(ActionEvent e) 
+					    {
+					    	if (e.getActionCommand().equals("normalGame"))
+							{
+					    		GAMEMODE = 0;
+								
+								System.out.println("Gamemode set to " + GAMEMODE);
+								
+								hideGamemodeSelector();
+								playerSelector();
+							}
+						}
+				  }
+				);
+		normalGamemodeBTN.setVisible(true);
+		
+		gameFrame.add(lblGameSelectorTitle);
+		gameFrame.add(normalGamemodeBTN);
+	}
+	
+	public void hideGamemodeSelector()
+	{
+		lblGameSelectorTitle.setVisible(false);
+		normalGamemodeBTN.setVisible(false);
+	}
+	
+	public void playerSelector()
+	{
+		lblGamePlayerSTitle = new JLabel(imgPlMain);
+		lblGamePlayerSTitle.setSize(300, 45);
+		lblGamePlayerSTitle.setFont(new Font("Serif", Font.PLAIN, 35));
+		lblGamePlayerSTitle.setLocation(290, 100);
+		lblGamePlayerSTitle.setOpaque(false);
+		lblGamePlayerSTitle.setVisible(true);
+		
+		player1BTN = new JButton(imgOnePlMain);
+		player1BTN.setSize(179, 40);
+		player1BTN.setFont(new Font("Serif", Font.PLAIN, 25));
+		player1BTN.setLocation(345, 170);
+		player1BTN.setFocusable(false);
+		player1BTN.setActionCommand("oneplayerGame");
+		player1BTN.addActionListener(
+				  new ActionListener() 
+						{
+					    public void actionPerformed(ActionEvent e) 
+					    {
+					    	if (e.getActionCommand().equals("oneplayerGame"))
+							{
+					    		PLAYERS = 1;
+								
+								System.out.println("Selected " + PLAYERS + " player('s)");
+							}
+						}
+				  }
+				);
+		player1BTN.setVisible(true);
+		
+		player2BTN = new JButton(imgTwoPlMain);
+		player2BTN.setSize(179, 40);
+		player2BTN.setFont(new Font("Serif", Font.PLAIN, 25));
+		player2BTN.setLocation(345, 220);
+		player2BTN.setFocusable(false);
+		player2BTN.setActionCommand("twoplayersGame");
+		player2BTN.addActionListener(
+				  new ActionListener() 
+						{
+					    public void actionPerformed(ActionEvent e) 
+					    {
+					    	if (e.getActionCommand().equals("twoplayersGame"))
+							{
+					    		PLAYERS = 2;
+								
+								System.out.println("Selected " + PLAYERS + " player('s)");
+							}
+						}
+				  }
+				);
+		player2BTN.setVisible(true);
+		
+		gameFrame.add(lblGamePlayerSTitle);
+		gameFrame.add(player1BTN);
+		gameFrame.add(player2BTN);
+	}
+	
+	public void hidePlayerSelector()
+	{
+		lblGamePlayerSTitle.setVisible(false);
+		player1BTN.setVisible(false);
+		player2BTN.setVisible(false);
+	}
+	
+	public void setUpMap1()
+	{
+		//Hides all menus
+		lblGameSelectorTitle.setVisible(false);
+		normalGamemodeBTN.setVisible(false);
+		lblGamePlayerSTitle.setVisible(false);
+		player1BTN.setVisible(false);
+		player2BTN.setVisible(false);
+		lblGameTitle.setVisible(false);
+		startGame.setVisible(false);
+		leaderboardMainBTN.setVisible(false);
+		settingsMainBTN.setVisible(false);
+		
+		//Map Setup
+		
+	}
+	
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void actionPerformed(ActionEvent event) {
+		
+		
+		
+		checkCollisions();
+	}
+	
+	public void checkCollisions()
+	{
+		
 	}
 	
 	public void MenuBar()
@@ -178,7 +399,18 @@ public class Game implements ActionListener, KeyListener {
 						    {
 						    	if (e.getActionCommand().equals("map1"))
 								{
-						    		setUpMap1();
+						    		//Hides all menus
+						    		lblGameSelectorTitle.setVisible(false);
+						    		normalGamemodeBTN.setVisible(false);
+						    		lblGamePlayerSTitle.setVisible(false);
+						    		player1BTN.setVisible(false);
+						    		player2BTN.setVisible(false);
+						    		lblGameTitle.setVisible(false);
+						    		startGame.setVisible(false);
+						    		leaderboardMainBTN.setVisible(false);
+						    		settingsMainBTN.setVisible(false);
+						    		
+						    		
 								}
 						    }
 						  }
@@ -186,218 +418,5 @@ public class Game implements ActionListener, KeyListener {
 		menu.add(menuItem);
 		gameFrame.setJMenuBar(menuBar);
 		rbMenuItem2.setEnabled(true);
-	}
-	
-	public void StartScreen()
-	{
-		lblGameTitle = new JLabel(imgLogoMain);
-		lblGameTitle.setSize(310, 70);
-		lblGameTitle.setFont(new Font("Serif", Font.PLAIN, 39));
-		lblGameTitle.setLocation(280, 100);
-		lblGameTitle.setOpaque(false);
-		lblGameTitle.setVisible(true);
-		
-		startGame = new JButton(imgStartMain);
-		startGame.setSize(181, 46);
-		startGame.setFont(new Font("Serif", Font.PLAIN, 25));
-		startGame.setLocation(345, 170);
-		startGame.setFocusable(false);
-		startGame.setActionCommand("startGame");
-		startGame.addActionListener(
-				  new ActionListener() 
-						{
-					    public void actionPerformed(ActionEvent e) 
-					    {
-					    	if (e.getActionCommand().equals("startGame"))
-							{
-					    		hideStartScreen();
-								gamemodeSelector();
-							}
-						}
-				  }
-				);
-		startGame.setVisible(true);
-		
-		leaderboardMainBTN = new JButton(imgLeadMain);
-		leaderboardMainBTN.setFont(new Font("Serif", Font.PLAIN, 25));
-		leaderboardMainBTN.setSize(181, 46);
-		leaderboardMainBTN.setLocation(345, 219);
-		leaderboardMainBTN.setFocusable(false);
-		leaderboardMainBTN.setActionCommand("leaderBoard");
-		//startGame.addActionListener(this);
-		leaderboardMainBTN.setVisible(true);
-		
-		settingsMainBTN = new JButton(imgSettMain);
-		settingsMainBTN.setFont(new Font("Serif", Font.PLAIN, 25));
-		settingsMainBTN.setSize(179, 40);
-		settingsMainBTN.setLocation(345, 269);
-		settingsMainBTN.setFocusable(false);
-		settingsMainBTN.setActionCommand("settings");
-		//startGame.addActionListener(this);
-		settingsMainBTN.setVisible(true);
-		
-		//Add Items to Frame
-		gameFrame.add(lblGameTitle);
-		gameFrame.add(startGame);
-		gameFrame.add(leaderboardMainBTN);
-		gameFrame.add(settingsMainBTN);
-	}
-	
-	public void hideStartScreen()
-	{
-		lblGameTitle.setVisible(false);
-		startGame.setVisible(false);
-		leaderboardMainBTN.setVisible(false);
-		settingsMainBTN.setVisible(false);
-	}
-	
-	public void gamemodeSelector()
-	{
-		lblGameSelectorTitle = new JLabel("Select Gamemode");
-		lblGameSelectorTitle.setSize(300, 45);
-		lblGameSelectorTitle.setFont(new Font("Serif", Font.PLAIN, 35));
-		lblGameSelectorTitle.setLocation(320, 100);
-		lblGameSelectorTitle.setOpaque(false);
-		lblGameSelectorTitle.setVisible(true);
-		
-		normalGamemodeBTN = new JButton("Normal");
-		normalGamemodeBTN.setSize(179, 40);
-		normalGamemodeBTN.setFont(new Font("Serif", Font.PLAIN, 25));
-		normalGamemodeBTN.setLocation(345, 170);
-		normalGamemodeBTN.setFocusable(false);
-		normalGamemodeBTN.setActionCommand("normalGame");
-		normalGamemodeBTN.addActionListener(
-				  new ActionListener() 
-						{
-					    public void actionPerformed(ActionEvent e) 
-					    {
-					    	if (e.getActionCommand().equals("normalGame"))
-							{
-					    		GAMEMODE = 0;
-								
-								System.out.println("Gamemode set to " + GAMEMODE);
-								
-								hideGamemodeSelector();
-								playerSelector();
-							}
-						}
-				  }
-				);
-		normalGamemodeBTN.setVisible(true);
-		
-		gameFrame.add(lblGameSelectorTitle);
-		gameFrame.add(normalGamemodeBTN);
-	}
-	
-	public void hideGamemodeSelector()
-	{
-		lblGameSelectorTitle.setVisible(false);
-		normalGamemodeBTN.setVisible(false);
-	}
-	
-	public void playerSelector()
-	{
-		lblGamePlayerSTitle = new JLabel("How many Players?");
-		lblGamePlayerSTitle.setSize(300, 45);
-		lblGamePlayerSTitle.setFont(new Font("Serif", Font.PLAIN, 35));
-		lblGamePlayerSTitle.setLocation(310, 100);
-		lblGamePlayerSTitle.setOpaque(false);
-		lblGamePlayerSTitle.setVisible(true);
-		
-		player1BTN = new JButton("One Player");
-		player1BTN.setSize(179, 40);
-		player1BTN.setFont(new Font("Serif", Font.PLAIN, 25));
-		player1BTN.setLocation(345, 170);
-		player1BTN.setFocusable(false);
-		player1BTN.setActionCommand("oneplayerGame");
-		player1BTN.addActionListener(
-				  new ActionListener() 
-						{
-					    public void actionPerformed(ActionEvent e) 
-					    {
-					    	if (e.getActionCommand().equals("oneplayerGame"))
-							{
-					    		PLAYERS = 1;
-								
-								System.out.println("Selected " + PLAYERS + " player('s)");
-							}
-						}
-				  }
-				);
-		player1BTN.setVisible(true);
-		
-		player2BTN = new JButton("Two Player's");
-		player2BTN.setSize(179, 40);
-		player2BTN.setFont(new Font("Serif", Font.PLAIN, 25));
-		player2BTN.setLocation(345, 220);
-		player2BTN.setFocusable(false);
-		player2BTN.setActionCommand("twoplayersGame");
-		player2BTN.addActionListener(
-				  new ActionListener() 
-						{
-					    public void actionPerformed(ActionEvent e) 
-					    {
-					    	if (e.getActionCommand().equals("twoplayersGame"))
-							{
-					    		PLAYERS = 2;
-								
-								System.out.println("Selected " + PLAYERS + " player('s)");
-							}
-						}
-				  }
-				);
-		player2BTN.setVisible(true);
-		
-		gameFrame.add(lblGamePlayerSTitle);
-		gameFrame.add(player1BTN);
-		gameFrame.add(player2BTN);
-	}
-	
-	public void hidePlayerSelector()
-	{
-		lblGamePlayerSTitle.setVisible(false);
-		player1BTN.setVisible(false);
-		player2BTN.setVisible(false);
-	}
-	
-	public void setUpMap1()
-	{
-		hideStartScreen();
-		hideGamemodeSelector();
-		hidePlayerSelector();
-		
-		map1();
-	}
-	
-	public void map1()
-	{
-		
-	}
-	
-	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void actionPerformed(ActionEvent event) {
-		
-		
-		
-		checkCollisions();
-	}
-	
-	public void checkCollisions()
-	{
-		
 	}
 }
